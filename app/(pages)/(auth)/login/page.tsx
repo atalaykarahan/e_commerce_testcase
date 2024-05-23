b620fd281a70affa11efcada104be715078d102d
atalaykarahan <krhnatalay@gmail.com>
2024-05-23 23:53:16 +0300

    giriş yap sayfası demo düzeyde bitti

--- app/(pages)/(auth)/login/page.tsx
+++ app/(pages)/(auth)/login/page.tsx
@@ -10,29 +10,29 @@ const LoginPage = () => {
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Login</h1>
+            <h1 className="text-3xl font-bold">Giriş Yap</h1>
             <p className="text-balance text-muted-foreground">
-              Enter your email below to login to your account
+              Hesabına giriş yapmak için bilgilerini gir
             </p>
           </div>
           <div className="grid gap-4">
             <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
+              <Label htmlFor="email">E Posta</Label>
               <Input
                 id="email"
                 type="email"
-                placeholder="m@example.com"
+                placeholder="m@ornek.com"
                 required
               />
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
+                <Label htmlFor="password">Şifre</Label>
                 <Link
                   href="/forgot-password"
                   className="ml-auto inline-block text-sm underline"
                 >
-                  Forgot your password?
+                  Şifreni mi unuttun?
                 </Link>
               </div>
               <Input id="password" type="password" required />
@@ -40,12 +40,9 @@ const LoginPage = () => {
             <Button type="submit" className="w-full">
               Login
             </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
           </div>
           <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
+            Hesabın yok mu kayıt ol.{" "}
             <Link href="#" className="underline">
               Sign up
             </Link>
